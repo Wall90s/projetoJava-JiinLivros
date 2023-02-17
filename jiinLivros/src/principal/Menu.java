a6bb717eddac9ea0797d8a656c489d95ca0a5338
Wallace <wallacesb@outlook.com.br>
2023-02-17 13:52:29 -0300

    Iniciando projeto e Adicionando esboco do Menu

---
+++ jiinLivros/src/principal/Menu.java
@@ -0,0 +1,32 @@
+package principal;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		int opcao;
+
+		// while(true) {
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("                Sistema gerenciador de biblioteca                ");
+		System.out.println("-----------------------------------------------------------------");
+		System.out.println("                                                                 ");
+		System.out.println("           Olá < fulane >, digite a opção que deseja:            ");
+		System.out.println("                                                                 ");
+		System.out.println("                                                                 ");
+		System.out.println("           < 1 >  Consultar livro                                ");
+		System.out.println("           < 2 >  Cadastrar livro                                ");
+		System.out.println("           < 3 >  Atualizar  livro                               ");
+		System.out.println("           < 4 >  Excluir livro                                  ");
+		System.out.println("           < 0 >  Sair                                           ");
+		System.out.println("                                                                 ");
+		System.out.println("-----------------------------------------------------------------");
+
+		// }
+
+	}
+
+}
