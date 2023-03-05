d937f7d56b81eaef1bb692beeee400e47e4169b0
Samantha Alexandra <lexalaska31@gmail.com>
2023-03-05 17:42:27 -0300

    Adicionando a Pasta Site

---
+++ jiinLivros/src/principal/controller/LivroController.java
@@ -0,0 +1,154 @@
+package principal.controller;
+
+import java.util.ArrayList;
+
+import principal.model.Livro;
+import principal.repository.LivroRepository;
+
+public class LivroController implements LivroRepository {
+
+	private ArrayList<Livro> listaLivros = new ArrayList<Livro>();
+	private int numero = 1;
+	
+	@Override
+	public void consultarPorTitulo(String titulo) {
+		ArrayList<Livro> livrosEncontrados = new ArrayList<Livro>();
+
+		for (var livro : listaLivros) {
+			String livroTitulo = livro.getTitulo();
+			if (livroTitulo.equalsIgnoreCase(titulo)) {
+				livrosEncontrados.add(livro);
+			}
+		}
+
+		if (livrosEncontrados.size() == 0) {
+			System.out.println("\nLivro não encontrado na lista\n");
+		} else {
+			for (Livro livro : livrosEncontrados) {
+				livro.visualizar();
+			}
+		}
+
+	}
+
+	@Override
+	public void consultarPorGenero(String genero) {
+		ArrayList<Livro> livrosEncontrados = new ArrayList<Livro>();
+
+		for (var livro : listaLivros) {
+			String livroGenero = livro.getGenero();
+			if (livroGenero.equalsIgnoreCase(genero)) {
+				livrosEncontrados.add(livro);
+			}
+		}
+
+		if (livrosEncontrados.size() == 0) {
+			System.out.println("\nGênero não encontrado na lista\n");
+		} else {
+			for (Livro livro : livrosEncontrados) {
+				livro.visualizar();
+			}
+		}
+
+	}
+
+	@Override
+	public void consultarPorAutor(String autor) {
+		ArrayList<Livro> livrosEncontrados = new ArrayList<Livro>();
+
+		for (var livro : listaLivros) {
+			String livroAutor = livro.getAutor();
+			if (livroAutor.equalsIgnoreCase(autor)) {
+				livrosEncontrados.add(livro);
+			}
+		}
+
+		if (livrosEncontrados.size() == 0) {
+			System.out.println("\nAutor não encontrado na lista\n");
+		} else {
+			for (Livro livro : livrosEncontrados) {
+				livro.visualizar();
+			}
+		}
+
+	}
+
+	@Override
+	public void consultarPorIsbn(String isbn) {
+		ArrayList<Livro> livrosEncontrados = new ArrayList<Livro>();
+
+		for (var livro : listaLivros) {
+			String livroIsbn = livro.getIsbn();
+			if (livroIsbn.equalsIgnoreCase(isbn)) {
+				livrosEncontrados.add(livro);
+			}
+		}
+
+		if (livrosEncontrados.size() == 0) {
+			System.out.println("\nISBN não encontrado na lista\n");
+		} else {
+			for (Livro livro : livrosEncontrados) {
+				livro.visualizar();
+			}
+		}
+
+	}
+
+	@Override
+	public void cadastrarLivro(Livro livro) {
+		listaLivros.add(livro);
+	}
+
+	@Override
+	public void listarLivros() {
+		
+		if(listaLivros.size() != 0) {
+			for (Livro livro : listaLivros) {
+			livro.visualizar();
+			}
+		} else {
+			System.out.println("\nA Lista está vazia!\n");
+		}
+
+	}
+
+	@Override
+	public void atualizarLivro(Livro livro) {
+		var buscaLivro = buscarNaCollection(livro.getCodigo());
+		
+		listaLivros.set(listaLivros.indexOf(buscaLivro), livro);
+		System.out.println("\nO livro: " + livro.getTitulo() + " com o código " + livro.getCodigo() +  " foi atualizado com sucesso!\n");
+
+	}
+
+	@Override
+	public void excluirLivro(int codigo) {
+		var buscaLivro = buscarNaCollection(codigo);
+		
+		if(buscaLivro != null) {
+			if(listaLivros.remove(buscaLivro) == true) {
+				System.out.print("\nO Livro com o " + codigo + " foi excluido com sucesso!\n");
+			}
+		} else {
+			System.out.println("\nO Livro com o código: " + codigo + " não foi encontrado!\n");
+		}
+	}
+	
+	public Livro buscarNaCollection(int codigo) {
+		for (var livro : listaLivros)
+			if (livro.getCodigo() == codigo) {
+				return livro;
+			}
+		return null;
+	}
+	
+	public int gerarCodigo() {		
+		if (listaLivros.size() == 0 ) {
+			return numero;
+		}else {
+			numero++;
+			return numero;
+		}
+	}
+
+}
